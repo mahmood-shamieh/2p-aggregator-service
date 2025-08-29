dcae30489796ef713b965fa15707ccaba9150ea2
Mahmood Shamie <mahmoodshamiehh2000@gmail.com>
2025-08-29 20:45:32 +0300

    init commit

---
+++ src/main/java/agency/twoPdigital/vod/aggregator/AggregatorService/AggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package agency.twoPdigital.vod.aggregator.AggregatorService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agency/twoPdigital/vod/aggregator/AggregatorService/AggregatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package agency.twoPdigital.vod.aggregator.AggregatorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
